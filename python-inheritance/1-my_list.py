fe35302d96f2afefa0a266797a4a1982bbce7b19
Jordan <jordan.arguelles.ja@gmail.com>
2024-01-23 18:38:25 -0800

    update

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains the MyList Class."""
+
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """intializes the object"""
+        super().__init__()
+
+        def print_sorted(self):
+            """prints the sorted list"""
+            print(sorted(self))
