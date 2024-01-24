ab8e9300872c5044723dda715f34e540c8fe3474
Jordan <jordan.arguelles.ja@gmail.com>
2024-01-23 19:33:11 -0800

    update

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module containing IO functions."""
+
+
+def write_file(filename="", text=""):
+    """Write a UTF-8 encoded text to a file.
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The content to store in the file.
+    Returns:
+        int: The number of the characters written.
+    """
+    n = 0
+    with open(filename, mode='w', encoding'utf-8') as file:
+        n = file.write(text)
+    return n
