de95b7b1f1d19a8eb0fe7d288d08c8a4b3e491fc
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2024-02-20 22:31:37 -0600

    creating database and new cities utilizing foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
---create database and new cities table utilizing foreign key
+-- create database and new cities table utilizing foreign key
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE table IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
