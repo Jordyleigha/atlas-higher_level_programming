beef5f620913606c732706245f5e0eb8bd85e659
Jordyleigha <jordana@Jordanslaptop>
2024-03-26 21:28:10 -0500

    writing my script

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+let max = 0;
+let res = 0;
+
+for (let i = 2; i < argv.length; i++) {
+  const value = Number(argv[i]);
+  if (isNaN(value)) {
+    continue;
+  } else if (value > max) {
+    [res, max] = [max, value];
+  } else if (value < max && value > res) {
+    res = value;
+  }
+}
+
+console.log(res);
