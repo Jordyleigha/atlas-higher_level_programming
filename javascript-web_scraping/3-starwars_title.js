bab5c4015d6a04d6523aa5031896953f50016d50
Jordyleigha <jordana@Jordanslaptop>
2024-03-28 15:01:27 -0500

    fixing

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,9 @@
 const request = require('request');
 const id = process.argv[2];
 request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
+  if (error) {
+    console.error(error);
+  } else {
     const json = JSON.parse(body);
     console.log(json.title);
   }
