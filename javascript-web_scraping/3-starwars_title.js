0824f23fdcbbf5790483899038854ba435d04586
Jordyleigha <jordana@Jordanslaptop>
2024-03-28 14:53:45 -0500

    forgot to identifiy id

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
+const id = process.argv[2];
 request('http:swapi.co/api/films/' + id + '/', function (error, response, body) {
   if (error == null) {
     const json = JSON.parse(body);
