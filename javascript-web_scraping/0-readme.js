44fe12f3f0ceff6dab645ea1449f495d66eba926
Jordyleigha <jordana@Jordanslaptop>
2024-03-28 14:38:16 -0500

    fixing my script

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const filePath = process.argv[2];
-
-try {
-  const content = fs.readFileSynce(filePath, 'utf-8');
-  console.log(content);
-} catch (err) {
-  console.error(err);
-}
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
+    console.log(err);
+  } else {
+    console.log(contents);
+  }
+});
